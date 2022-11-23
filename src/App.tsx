39b65206f74660522b9b0edb5d9f56130b7b6073
josemlrod <mrjosemlrod@gmail.com>
2022-11-22 20:12:17 -0500

    create Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <h1 className="">Hello world!</h1>;
+  return (
+    <React.Fragment>
+      <Navbar />
+      <h1 className="">Hello world!</h1>
+    </React.Fragment>
+  );
 }
 
 export default App;
